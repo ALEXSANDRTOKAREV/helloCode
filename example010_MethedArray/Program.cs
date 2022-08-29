7cf3160238c50f3e962858160d0f309711f1ea6a
Tokarev Alex <Tokarev_alex93@mail.ru>
2022-08-29 22:15:01 +0300

    inrtomethedarray

---
+++ example010_MethedArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 34, 56, 4, 98, 56, 18, 78, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
